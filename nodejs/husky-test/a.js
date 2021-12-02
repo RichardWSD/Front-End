539d6124aa457f2382e58873e5e18c3e301d1df3
richard <wushideng@ifanr.com>
2021-12-02 09:58:05 +0800

    husky

---
+++ nodejs/husky-test/a.js
@@ -0,0 +1,10 @@
+// https://blog.csdn.net/weixin_44052136/article/details/120675806
+// 使用前先git init一个仓库，毕竟要提交才能看到效果
+
+console.log(11)
+console.log(11)
+console.log(11)
+console.log(11)
+console.log(11)
+console.log(11);
+// console.log(11);

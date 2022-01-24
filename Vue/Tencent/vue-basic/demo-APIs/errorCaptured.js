bd4b48645e1b68f8c106ec62daf9da1a8c98964a
richard <wushideng@ifanr.com>
2022-01-24 18:54:48 +0800

    m

--- Vue/Tencent/vue-basic/demo-APIs/errorCaptured.js
+++ Vue/Tencent/vue-basic/demo-APIs/errorCaptured.js
@@ -29,6 +29,13 @@ Vue.component('kitten', {
   }
 });
 
+Vue.config.errorHandler = function (err, vm, info) {
+  // handle error
+  // `info` 是 Vue 特定的错误信息，比如错误所在的生命周期钩子
+  // 只在 2.2.0+ 可用
+  console.error('errorHandler', err, vm, info);
+}
+
 const app = new Vue({
   el:'#app'
 })

4070792545f43fd5faf906fd2de525c83d768798
Richard Wu <shideng.wu@dorabot.com>
2022-09-22 09:47:32 +0800

    m

--- Vue/Tencent/vue-basic/native用法/app.js
+++ Vue/Tencent/vue-basic/native用法/app.js
@@ -1,12 +1,13 @@
-Vue.component('my-component', {
-    　　template: `<a href='#'>click me</a>`
-    })
+Vue.component("my-component", {
+  template: `<a href='#' v-on="$listeners">click me</a>`,
+  // template: `<a href='#'>click me</a>`,
+});
 
 new Vue({
-    　　el: '#app',
-    　　methods: {
-    　　　　clickFun: function(){
-    　　　　　　console.log("11111")
-    　　　　}
-    　　}
-    })
+  el: "#app",
+  methods: {
+    clickFun: function () {
+      console.log("11111");
+    },
+  },
+});
